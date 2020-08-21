aad9900da736f19ca028863dff11fb01fedbf182
aredkovsky <aredkovsky@itransition.com>
2020-08-21 17:54:20 +0300

    Itra-0002: Added a description to the functions in the user service.

--- src/main/java/com/example/services/UserService.java
+++ src/main/java/com/example/services/UserService.java
@@ -4,9 +4,27 @@ import com.example.dto.User;
 
 public interface UserService {
 
+    /**
+     * Saving a new user
+     * @param firstName
+     * @param lastName
+     * @param email
+     * @param password
+     */
     void saveUser(String firstName, String lastName, String email, String password);
 
+    /**
+     * Search for a user by his ID
+     * @param id User's id
+     * @return User with this id
+     */
     User receiveUser(long id);
 
+    /**
+     * User authentication
+     * @param email User's email
+     * @param password User's password
+     * @return User if credentials are valid
+     */
     User receiveUser(String email, String password);
 }
